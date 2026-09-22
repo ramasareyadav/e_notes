b38e181befcea9f8138f4ed93ff0bd51cea88fa8
Ramasare Yadav <yadavrama096@gmail.com>
2026-09-23 04:07:20 +0530

    first commit

---
+++ e_notes/src/test/java/com/e_notes/ENotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e_notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ENotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
