d92a32aca6bed9b50c0d84662721bd29a8045278
Ramasare Yadav <yadavrama096@gmail.com>
2026-09-23 07:56:24 +0530

    delete and get method implementation

--- e_notes/src/main/java/com/e_notes/dto/CategoryDto.java
+++ e_notes/src/main/java/com/e_notes/dto/CategoryDto.java
@@ -11,7 +11,7 @@ import java.util.Date;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CategoryDto {
-   // private Long id;
+    private Long id;
     private String name;
     private String description;
     private Boolean isActive;
